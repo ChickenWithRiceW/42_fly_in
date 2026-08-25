840b2ae83e230cee15d447573484e20b3b9d184a
Achilles Scheufele <ascheufe@3-F-1.42heilbronn.de>
2026-08-25 14:52:58 +0200

    refactor: add config.py for classes cleaned up code

--- src/parser/parser.py
+++ src/parser/parser.py
@@ -1,77 +1,22 @@
-from pydantic import BaseModel, field_validator, ValidationError, Field, \
-    ConfigDict
-from typing import Optional
-from enum import Enum
+from __future__ import annotations
+from pydantic import ValidationError
+from .config import Config, Connection, Hub, HubMetadata
 
 
-# TODO: Will be changed into pygame colors
-class Color(Enum):
-    RED = "red"
-    GREEN = "green"
-    BLUE = "blue"
-    PURPLE = "purple"
-    BLACK = "black"
-    BROWN = "brown"
-    ORANGE = "orange"
-    MAROON = "maroon"
-    GOLD = "gold"
-    DARKRED = "darkred"
-    VIOLET = "violet"
-    CRIMSON = "crimson"
-    RAINBOW = "rainbow"
-
-
-class ZoneType(Enum):
-    NORMAL = "normal"
-    RESTRICTED = "restricted"
-    PRIORITY = "priority"
-    BLOCKED = "blocked"
-
-
-class HubMetadata(BaseModel):
-    model_config = ConfigDict(extra='forbid')
-    color: Optional[Color] = Color.GREEN
-    max_drones: Optional[int] = Field(ge=0, default=1)
-    zone_type: Optional[ZoneType] = ZoneType.NORMAL
-
-
-class Hub(BaseModel):
-    name: str
-    coordinate: tuple[int, int]
-    metadata: Optional[HubMetadata] = HubMetadata(
-        color=Color.BLUE, zone_type=ZoneType.NORMAL)
-
-    @field_validator('name', mode='after')
-    @classmethod
-    def _validation(cls, string: str) -> str:
-        if '-' in string or ' ' in string:
-            raise MapException("Name can't contain '-' or ' '")
-        return string
-
-
-class Connection(BaseModel):
-    from_zone: str
-    to_zone: str
-    max_link_capacity: Optional[int] = 1
-
-
-class MapException(Exception):
-    def __init__(self, msg: str) -> None:
-        super().__init__(msg)
-
-
-class Config(BaseModel):
-    nb_drones: int
-    start_hub: Hub
-    end_hub: Hub
-    zones: dict[str, Hub]
-    connections: list[Connection]
+class FileData:
+    def __init__(self) -> None:
+        self.file_name: str
+        self.line: list[str]
+        self.line_nb: int
+        self.hubs: dict[str, Hub]
+        self.connections: list[Connection]
 
 
 class ConfigLoader:
     @classmethod
     def config_loader(cls, file_name: str) -> Config | None:
-        """Load config from provided file name if possible
+        """Load config from provided file name if possible.
+        Checks for errors prints them and returns None if failed
 
         Args:
             file_name (str): Filename of config file
@@ -82,8 +27,7 @@ class ConfigLoader:
         file = cls._loader(file_name)
         if file is None:
             return None
-        map_val = cls._parser(file)
-        return map_val
+        return cls._parser(file, file_name)
 
     @staticmethod
     def _loader(file_name: str) -> list[tuple[list[str], int]] | None:
@@ -112,7 +56,20 @@ class ConfigLoader:
         return split
 
     @classmethod
-    def _parser(cls, loaded_file: list[tuple[list[str], int]]) -> Config | None:
+    def _parser(cls,
+                loaded_file: list[tuple[list[str], int]],
+                file_name: str) -> Config | None:
+        """Parse given list of tuples to check for malformed lines etc.
+
+        Args:
+            loaded_file (list[tuple[list[str], int]]): Read file from
+                config_loader
+            file_name (str): Config filename
+
+        Returns:
+            Config | None: Returns either config object or None if errors
+                occurred
+        """
         nb_drones: int | None = None
         start_hub: Hub | None = None
         end_hub: Hub | None = None
@@ -120,212 +77,231 @@ class ConfigLoader:
         hubs: dict[str, Hub] = {}
         connections: list[Connection] = []
 
+        data = FileData()
+        data.file_name = file_name
+        data.hubs = hubs
+        data.connections = connections
+
         # Index 0 is the key name.
         for line, line_nb in loaded_file:
+            data.line = line
+            data.line_nb = line_nb
+
             match line[0]:
                 case "nb_drones:":
-                    if nb_drones is not None:
-                        print(
-                            f"Line:{line_nb} Error: nb_drones already defined")
-                        return None
-
-                    nb_drones = cls._nb_drones_parse(line, line_nb)
-                    if nb_drones is None:
+                    if not (nb_drones := cls._drone_parser(data, nb_drones)):
                         return None
 
                 case "start_hub:":
-                    if start_hub is not None:
-                        print(
-                            f"Line:{line_nb} Error: start_hub already defined")
-                        return None
-                    start_hub = cls._hub_parser(line, line_nb, hubs)
-                    if start_hub is None:
+                    if not (start_hub := cls._hub_parser(data, start_hub)):
                         return None
                     hubs.update({start_hub.name: start_hub})
 
                 case "end_hub:":
-                    if end_hub is not None:
-                        print(
-                            f"Line:{line_nb} Error: end_hub already defined")
-                        return None
-                    end_hub = cls._hub_parser(line, line_nb, hubs)
-                    if end_hub is None:
+                    if not (end_hub := cls._hub_parser(data, end_hub=end_hub)):
                         return None
                     hubs.update({end_hub.name: end_hub})
 
                 case "hub:":
-                    tmp_h = cls._hub_parser(line, line_nb, hubs)
-                    if tmp_h is None:
+                    if not (hub := cls._hub_parser(data)):
                         return None
-                    hubs.update({tmp_h.name: tmp_h})
+                    hubs.update({hub.name: hub})
 
                 case "connection:":
-                    tmp_c = cls._connection_parse(line, line_nb)
-                    if tmp_c is None:
+                    if not (connection := cls._connection_parser(data)):
                         return None
-                    connections.append(tmp_c)
+                    connections.append(connection)
 
                 case _:
                     if line[0]:
                         print(
-                            f"Line:{line_nb} Error: Unknown key '{line[0]}'")
+                            f"{file_name}:{line_nb} "
+                            f"Error: Unknown key '{line[0]}'")
                         return None
 
         try:
-            map_val = Config(
+            return Config(
                 nb_drones=nb_drones,
                 start_hub=start_hub,
                 end_hub=end_hub,
                 zones=hubs,
                 connections=connections
             )
-
         except ValidationError as e:
             for error in e.errors():
                 print(f"Error: Wrong input '{error['input']}'. {error['msg']}")
             return None
-        return cls._verify(map_val)
 
     @staticmethod
-    def _nb_drones_parse(input: list[str], line_nb: int) -> None | int:
-        try:
-            number = int(input[1])
-        except ValueError as e:
-            print(f"Line:{line_nb} {e}")
+    def _drone_parser(data: FileData,
+                      nb_drones: int | None) -> None | int:
+        if nb_drones is not None:
+            print(
+                f"{data.file_name}:{data.line_nb} "
+                "Error: nb_drones already defined")
             return None
 
-        if number < 0:
+        try:
+            number = int(data.line[1])
+        except ValueError as e:
+            print(f"{data.file_name}:{data.line_nb} {e}")
+            return None
+
+        if number < 1:
+            print(f"{data.file_name}:{data.line_nb} "
+                  "Error: nb_drones needs to a positive integer")
             return None
         return number
 
     @classmethod
-    def _hub_parser(
-        cls,
-        input: list[str],
-        line_nb: int,
-            hubs: dict[str, Hub]) -> Hub | None:
+    def _hub_parser(cls,
+                    data: FileData,
+                    start_hub: Hub | None = None,
+                    end_hub: Hub | None = None) -> Hub | None:
 
         metadata: HubMetadata | None = None
 
-        if "-" in input[1]:
-            print(f"Line:{line_nb} Error: Can't have '-' in name")
+        if start_hub is not None:
+            print(
+                f"{data.file_name}:{data.line_nb} "
+                "Error: start_hub already defined")
             return None
 
-        if len(input) == 5:
-            metadata = cls._hub_metadata_parse(input[4], line_nb)
+        if end_hub is not None:
+            print(
+                f"{data.file_name}:{data.line_nb} "
+                "Error: end_hub already defined")
+            return None
+
+        if "-" in data.line[1]:
+            print(f"{data.file_name}:{data.line_nb} "
+                  "Error: Can't have '-' in name")
+            return None
+
+        if len(data.line) == 5:
+            metadata = cls._hub_metadata_parse(data)
             if metadata is None:
                 return None
         try:
-            tmp = Hub(
-                name=input[1],
-                coordinate=(input[2], input[3]),
+            hub = Hub(
+                name=data.line[1],
+                coordinate=(data.line[2], data.line[3]),
                 metadata=metadata
             )
-            if tmp.name in hubs:
-                print(f"Line:{line_nb} Error: Zone duplicate found")
+
+            if hub.name in data.hubs:
+                print(f"{data.file_name}:{data.line_nb} "
+                      "Error: Zone duplicate found")
                 return None
-            return tmp
+            return hub
         except ValidationError as e:
             for error in e.errors():
-                print(f"Line:{line_nb} Error: Wrong input \
-                      '{error['input']}'. {error['msg']}")
+                print(f"{data.file_name}:{data.line_nb} "
+                      f"Error: Wrong input '{error['input']}'. {error['msg']}")
             return None
 
     @classmethod
-    def _connection_parse(
-        cls, input: list[str],
-            line_nb: int) -> Connection | None:
+    def _connection_parser(cls, data: FileData) -> Connection | None:
+
         max_link_capacity: dict[str, str] | None = {}
 
-        if len(input) > 3:
-            print(f"Line:{line_nb} Error: Connection syntax invalid")
+        if len(data.line) > 3:
+            print(f"{data.file_name}:{data.line_nb} "
+                  "Error: Connection syntax invalid")
             return None
-        split = input[1].split('-', maxsplit=3)
+        split = data.line[1].split('-', maxsplit=3)
 
         if len(split) < 2:
-            print(f"Line:{line_nb} Error: Connection syntax invalid")
+            print(f"{data.file_name}:{data.line_nb} "
+                  "Error: Connection syntax invalid")
             return None
 
-        if len(input) == 3:
-            max_link_capacity = cls._metadata_parse(input[2], line_nb)
+        if len(data.line) == 3:
+            max_link_capacity = cls._metadata_parse(data, data.line[2])
         if max_link_capacity is None:
             return None
         try:
-            return Connection(
+            connection = Connection(
                 from_zone=split[0],
                 to_zone=split[1],
                 **max_link_capacity)
 
+            if not cls._connection_parser_helper(data, connection):
+                return None
+
+            return connection
+
         except ValidationError as e:
             for error in e.errors():
-                print(f"Line:{line_nb} Error: Wrong input \
-                      '{error['input']}'. {error['msg']}")
+                print(f"{data.file_name}:{data.line_nb} "
+                      "Error: Wrong input "
+                      f"'{error['input']}'. {error['msg']}")
             return None
 
     @staticmethod
-    def _metadata_parse(input: str, line_nb: int) -> dict[str, str] | None:
+    def _metadata_parse(data: FileData, input: str) -> dict[str, str] | None:
         arguments = {}
+
         if not input.startswith('[') or not input.endswith(']'):
-            print(f"Line:{line_nb} Error: Metadata syntax wrong")
+            print(f"{data.file_name}:{data.line_nb} "
+                  "Error: Metadata syntax wrong")
             return None
 
         input = input.removeprefix('[').removesuffix(']')
         split = input.split(" ", maxsplit=2)
 
-        for data in split:
+        for part in split:
             # No splitting possible
-            if '=' not in data:
-                print(f"Line:{line_nb} Error: Format should be: Key=Value")
+            if '=' not in part:
+                print(f"{data.file_name}:{data.line_nb} "
+                      "Error: Format should be: Key=Value")
                 return None
 
-            k, v = data.split("=", maxsplit=2)
-            arguments[k] = v
+            key, value = part.split("=", maxsplit=2)
+            arguments[key] = value
         return arguments
 
     @classmethod
-    def _hub_metadata_parse(
-        cls, input: str,
-            line_nb: int) -> HubMetadata | None:
+    def _hub_metadata_parse(cls,
+                            data: FileData) -> HubMetadata | None:
         metadata_arguments: dict[str, str] | None
 
-        metadata_arguments = cls._metadata_parse(input, line_nb)
+        metadata_arguments = cls._metadata_parse(data, data.line[4])
         if metadata_arguments is None:
             return None
         try:
             return HubMetadata(**metadata_arguments)
         except ValidationError as e:
             for error in e.errors():
-                print(f"Line:{line_nb} Error: Wrong input \
-                      '{error['input']}'. {error['msg']}")
+                if error["type"] == "extra_forbidden":
+                    print(f"{data.file_name}:{data.line_nb} "
+                          "Error: Metadata keyword no match "
+                          f"'{error["loc"][0]}'")
+                else:
+                    print(f"{data.file_name}:{data.line_nb} "
+                          "Error: Wrong input "
+                          f"{error["loc"][0]} = '{error['input']}'. "
+                          f"{error['msg']}")
             return None
 
-    # TODO: Could put this into connections themselfs.
     @staticmethod
-    def _verify(map_val: Config) -> Config | None:
-        seen_con = set()
+    def _connection_parser_helper(data: FileData, sel: Connection) -> bool:
+        if sel.from_zone not in data.hubs \
+                or sel.to_zone not in data.hubs:
+            print(f"{data.file_name}:{data.line_nb} "
+                  "Error: Connection point not found")
+            return False
 
-        for connection in map_val.connections:
-            if connection.from_zone not in map_val.zones \
-                    or connection.to_zone not in map_val.zones:
-                print(f"Error: Connection point not found \
-                      '{connection.from_zone}'")
-                return None
+        if sel.from_zone == sel.to_zone:
+            print(f"{data.file_name}:{data.line_nb} "
+                  "Error: Connection can't connect to itself "
+                  f"'{sel.from_zone}-{sel.from_zone}'")
+            return False
 
-            if connection.from_zone == connection.to_zone:
-                print(f"Error: Connection can't connect to itself \
-                      '{connection.from_zone}-{connection.from_zone}'")
-                return None
-
-            # TODO: Could be improved in performance
-            if (connection.from_zone, connection.to_zone) in seen_con \
-                    or (connection.to_zone, connection.from_zone) in seen_con:
-                print("Error: Duplicated connection found "
-                      f"'{connection.from_zone}-{connection.to_zone}'")
-                return None
-            else:
-                seen_con.add((connection.from_zone, connection.to_zone))
-        return map_val
-
-
-if __name__ == "__main__":
-    ConfigLoader.config_loader("example_map.txt")
+        for con in data.connections:
+            if {sel.from_zone, sel.to_zone} == {con.from_zone, con.to_zone}:
+                print(f"{data.file_name}:{data.line_nb} "
+                      "Error: Duplicated connection found "
+                      f"'{sel.from_zone}-{sel.to_zone}'")
+                return False
+        return True
